e79ac42e6d70d4e89ef89e584250502f7f4b7308
test1 <31kiel12@gmail.com>
2024-09-18 13:44:29 +0300

    Добавьте файлы проекта.

---
+++ Loops3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double dist = 10;
+        double tDist = 0;
+        int day = 0;
+
+        while (tDist <= 100)
+        {
+            day++;
+            tDist += dist;
+            dist *= 1.1;
+        }
+
+        Console.WriteLine($"На {day}-й день суммарный пробег превысит 100 км.");
+    }
+}
